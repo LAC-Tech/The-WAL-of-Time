5d2b8e6a0fb178e55317ecb5d3241d5e4d7217e4
Lewis Campbell <info@lewiscampbell.tech>
2025-02-27 20:10:48 +1300

    translating into zig

--- src/dst.zig
+++ src/dst.zig
@@ -1,213 +1,213 @@
 //! Deterministic Simulation Tester
 
-const std = @import("std");
-const heap = std.heap;
-const math = std.math;
-const mem = std.mem;
-const posix = std.posix;
-const rand = std.rand;
-const testing = std.testing;
-
-const ArrayListUnmanged = std.ArrayListUnmanaged;
-const AutoHashMap = std.AutoHashMap;
-const PriorityQueue = std.PriorityQueue;
-
-const lib = @import("./root.zig");
-
-const c = @cImport({
-    @cInclude("tui.h");
-});
-
-const FileIO = struct {
-    fs: ArrayListUnmanged(ArrayListUnmanged(u8)),
-    events: Events,
-    allocator: std.mem.Allocator,
-
-    pub const FD = usize;
-    pub fn init(allocator: std.mem.Allocator) !@This() {
-        const events = Events.init(allocator, {});
-        //try events.ensureTotalCapacity(256);
-        return .{
-            .fs = try ArrayListUnmanged(ArrayListUnmanged(u8)).initCapacity(
-                allocator,
-                1_000_000,
-            ),
-            .events = events,
-            .allocator = allocator,
-        };
-    }
-
-    pub fn deinit(self: *@This()) void {
-        self.fs.deinit(self.allocator);
-        self.events.deinit();
-    }
-
-    const Event = struct { priority: u64, os_input: lib.FileOp(FD).Input };
-    const Events = PriorityQueue(Event, void, struct {
-        fn compare(_: void, a: Event, b: Event) math.Order {
-            return math.order(a.priority, b.priority);
-        }
-    }.compare);
-
-    /// Nothing ever happens... until we advance the state of the OS.
-    fn tick(self: *@This(), db: *lib.DB(FileIO)) !void {
-        const event = self.events.removeOrNull() orelse return;
-
-        switch (event.os_input.file_op) {
-            .create => {
-                const fd = self.fs.items.len;
-                const file = ArrayListUnmanged(u8){};
-                try self.fs.append(self.allocator, file);
-                db.receive_io(.{
-                    .task_id = event.os_input.task_id,
-                    .file_op = .{ .create = @intCast(fd) },
-                });
-            },
-            .read => |_| {
-                @panic("TODO: Read");
-            },
-            .append => |_| {
-                @panic("TODO: Append");
-            },
-            .delete => |_| {
-                @panic("TODO: Delete");
-            },
-        }
-    }
-
-    pub fn send(
-        self: *@This(),
-        ctx: *Context,
-        msg: lib.FileOp(FD).Input,
-    ) !void {
-        const event = .{
-            .priority = ctx.random.int(u64),
-            .os_input = msg,
-        };
-        try self.events.add(event);
-    }
-};
-
-const Context = struct {
-    random: std.Random,
-    stats: c.stats,
-};
-
-pub fn onReceive(ctx: *Context, res: lib.Res) void {
-    switch (res) {
-        .create => {
-            ctx.stats.os_files_created += 1;
-        },
-        else => @panic("TODO: collect more stats"),
-    }
-}
-
-const Simulator = struct {
-    node: lib.Node(FileIO, Context, onReceive),
-    ctx: Context,
-
-    fn init(allocator: mem.Allocator, random: std.Random) !@This() {
-        const ctx = .{
-            .random = random,
-            .stats = .{ .os_files_created = 0 },
-        };
-        return .{
-            .ctx = ctx,
-            .node = try lib.Node(FileIO, Context, onReceive).init(allocator, ctx),
-        };
-    }
-
-    fn deinit(self: *@This()) void {
-        self.node.deinit();
-    }
-
-    fn tick(self: *@This()) !void {
-        if (Config.create_file_chance > self.ctx.random.float(f64)) {
-            try self.node.create_stream("lmao");
-        }
-
-        try self.db.os.tick(&self.db);
-    }
-};
-
-// Configuration parameters for the DST
-// In one place for ease of tweaking
-const Config = struct {
-    const max_sim_time_in_ms: u64 = 1000 * 60 * 60 * 24; // 24 hours
-
-    // TODO: these chances are temporary; will later be driven by actual db
-    const create_file_chance = 0.1;
-    const delete_file_chance = 0.05;
-};
-
-fn live_simulation(sim: *Simulator) !void {
-    var tui = mem.zeroes(c.tui);
-    c.tui_init(&tui);
-    defer c.tui_deinit(&tui);
-
-    var time: u64 = 0;
-    while (time <= Config.max_sim_time_in_ms) : (time += 10) {
-        try sim.tick();
-        if (time % (1000 * 60) == 0) {
-            c.tui_sim_render(&tui, &sim.ctx.stats, time);
-        }
-    }
-}
-
-fn bg_simulation(sim: *Simulator) !void {
-    const phys_start_time = std.time.microTimestamp();
-
-    var time: u64 = 0;
-    while (time <= Config.max_sim_time_in_ms) : (time += 10) {
-        try sim.tick();
-    }
-
-    const phys_end_time = std.time.microTimestamp();
-    const phys_time_elapsed: f128 =
-        @floatFromInt(phys_end_time - phys_start_time);
-
-    std.debug.print(
-        "Stats: OS files created: {}\n",
-        .{sim.ctx.stats.os_files_created},
-    );
-    std.debug.print("Time: {} μs\n", .{phys_time_elapsed});
-}
-
-pub fn main() !void {
-    var args = std.process.args();
-    _ = args.skip();
-
-    const mode = args.next() orelse @panic("First arg must be 'live' or 'bg'");
-    const seed = if (args.next()) |arg|
-        try std.fmt.parseInt(u64, arg, 10)
-    else
-        std.crypto.random.int(u64);
-
-    std.debug.print("Seed = {}\n", .{seed});
-
-    var rng = rand.DefaultPrng.init(seed);
-    var gpa = heap.GeneralPurposeAllocator(.{}){};
-    var sim = try Simulator.init(gpa.allocator(), rng.random());
-
-    if (std.mem.eql(u8, mode, "bg")) {
-        try bg_simulation(&sim);
-    } else if (std.mem.eql(u8, mode, "live")) {
-        try live_simulation(&sim);
-    } else {
-        unreachable;
-    }
-
-    sim.deinit();
-    if (gpa.deinit() == .leak) {
-        std.debug.print("memory leak!!!!", .{});
-    }
-}
-
-test "sim lifetime" {
-    var rng = rand.DefaultPrng.init(0);
-    var sim = try Simulator.init(testing.allocator, rng.random());
-    for (0..1000) |_| {
-        try sim.tick();
-    }
-    defer sim.deinit();
-}
+//const std = @import("std");
+//const heap = std.heap;
+//const math = std.math;
+//const mem = std.mem;
+//const posix = std.posix;
+//const rand = std.rand;
+//const testing = std.testing;
+//
+//const ArrayListUnmanged = std.ArrayListUnmanaged;
+//const AutoHashMap = std.AutoHashMap;
+//const PriorityQueue = std.PriorityQueue;
+//
+//const lib = @import("./root.zig");
+//
+//const c = @cImport({
+//    @cInclude("tui.h");
+//});
+//
+//const FileIO = struct {
+//    fs: ArrayListUnmanged(ArrayListUnmanged(u8)),
+//    events: Events,
+//    allocator: std.mem.Allocator,
+//
+//    pub const FD = usize;
+//    pub fn init(allocator: std.mem.Allocator) !@This() {
+//        const events = Events.init(allocator, {});
+//        //try events.ensureTotalCapacity(256);
+//        return .{
+//            .fs = try ArrayListUnmanged(ArrayListUnmanged(u8)).initCapacity(
+//                allocator,
+//                1_000_000,
+//            ),
+//            .events = events,
+//            .allocator = allocator,
+//        };
+//    }
+//
+//    pub fn deinit(self: *@This()) void {
+//        self.fs.deinit(self.allocator);
+//        self.events.deinit();
+//    }
+//
+//    const Event = struct { priority: u64, os_input: lib.FileOp(FD).Input };
+//    const Events = PriorityQueue(Event, void, struct {
+//        fn compare(_: void, a: Event, b: Event) math.Order {
+//            return math.order(a.priority, b.priority);
+//        }
+//    }.compare);
+//
+//    /// Nothing ever happens... until we advance the state of the OS.
+//    fn tick(self: *@This(), db: *lib.DB(FileIO)) !void {
+//        const event = self.events.removeOrNull() orelse return;
+//
+//        switch (event.os_input.file_op) {
+//            .create => {
+//                const fd = self.fs.items.len;
+//                const file = ArrayListUnmanged(u8){};
+//                try self.fs.append(self.allocator, file);
+//                db.receive_io(.{
+//                    .task_id = event.os_input.task_id,
+//                    .file_op = .{ .create = @intCast(fd) },
+//                });
+//            },
+//            .read => |_| {
+//                @panic("TODO: Read");
+//            },
+//            .append => |_| {
+//                @panic("TODO: Append");
+//            },
+//            .delete => |_| {
+//                @panic("TODO: Delete");
+//            },
+//        }
+//    }
+//
+//    pub fn send(
+//        self: *@This(),
+//        ctx: *Context,
+//        msg: lib.FileOp(FD).Input,
+//    ) !void {
+//        const event = .{
+//            .priority = ctx.random.int(u64),
+//            .os_input = msg,
+//        };
+//        try self.events.add(event);
+//    }
+//};
+//
+//const Context = struct {
+//    random: std.Random,
+//    stats: c.stats,
+//};
+//
+//pub fn onReceive(ctx: *Context, res: lib.Res) void {
+//    switch (res) {
+//        .create => {
+//            ctx.stats.os_files_created += 1;
+//        },
+//        else => @panic("TODO: collect more stats"),
+//    }
+//}
+//
+//const Simulator = struct {
+//    node: lib.Node(FileIO, Context, onReceive),
+//    ctx: Context,
+//
+//    fn init(allocator: mem.Allocator, random: std.Random) !@This() {
+//        const ctx = .{
+//            .random = random,
+//            .stats = .{ .os_files_created = 0 },
+//        };
+//        return .{
+//            .ctx = ctx,
+//            .node = try lib.Node(FileIO, Context, onReceive).init(allocator, ctx),
+//        };
+//    }
+//
+//    fn deinit(self: *@This()) void {
+//        self.node.deinit();
+//    }
+//
+//    fn tick(self: *@This()) !void {
+//        if (Config.create_file_chance > self.ctx.random.float(f64)) {
+//            try self.node.create_stream("lmao");
+//        }
+//
+//        try self.db.os.tick(&self.db);
+//    }
+//};
+//
+//// Configuration parameters for the DST
+//// In one place for ease of tweaking
+//const Config = struct {
+//    const max_sim_time_in_ms: u64 = 1000 * 60 * 60 * 24; // 24 hours
+//
+//    // TODO: these chances are temporary; will later be driven by actual db
+//    const create_file_chance = 0.1;
+//    const delete_file_chance = 0.05;
+//};
+//
+//fn live_simulation(sim: *Simulator) !void {
+//    var tui = mem.zeroes(c.tui);
+//    c.tui_init(&tui);
+//    defer c.tui_deinit(&tui);
+//
+//    var time: u64 = 0;
+//    while (time <= Config.max_sim_time_in_ms) : (time += 10) {
+//        try sim.tick();
+//        if (time % (1000 * 60) == 0) {
+//            c.tui_sim_render(&tui, &sim.ctx.stats, time);
+//        }
+//    }
+//}
+//
+//fn bg_simulation(sim: *Simulator) !void {
+//    const phys_start_time = std.time.microTimestamp();
+//
+//    var time: u64 = 0;
+//    while (time <= Config.max_sim_time_in_ms) : (time += 10) {
+//        try sim.tick();
+//    }
+//
+//    const phys_end_time = std.time.microTimestamp();
+//    const phys_time_elapsed: f128 =
+//        @floatFromInt(phys_end_time - phys_start_time);
+//
+//    std.debug.print(
+//        "Stats: OS files created: {}\n",
+//        .{sim.ctx.stats.os_files_created},
+//    );
+//    std.debug.print("Time: {} μs\n", .{phys_time_elapsed});
+//}
+//
+//pub fn main() !void {
+//    var args = std.process.args();
+//    _ = args.skip();
+//
+//    const mode = args.next() orelse @panic("First arg must be 'live' or 'bg'");
+//    const seed = if (args.next()) |arg|
+//        try std.fmt.parseInt(u64, arg, 10)
+//    else
+//        std.crypto.random.int(u64);
+//
+//    std.debug.print("Seed = {}\n", .{seed});
+//
+//    var rng = rand.DefaultPrng.init(seed);
+//    var gpa = heap.GeneralPurposeAllocator(.{}){};
+//    var sim = try Simulator.init(gpa.allocator(), rng.random());
+//
+//    if (std.mem.eql(u8, mode, "bg")) {
+//        try bg_simulation(&sim);
+//    } else if (std.mem.eql(u8, mode, "live")) {
+//        try live_simulation(&sim);
+//    } else {
+//        unreachable;
+//    }
+//
+//    sim.deinit();
+//    if (gpa.deinit() == .leak) {
+//        std.debug.print("memory leak!!!!", .{});
+//    }
+//}
+//
+//test "sim lifetime" {
+//    var rng = rand.DefaultPrng.init(0);
+//    var sim = try Simulator.init(testing.allocator, rng.random());
+//    for (0..1000) |_| {
+//        try sim.tick();
+//    }
+//    defer sim.deinit();
+//}
--- src/root.zig
+++ src/root.zig
@@ -3,174 +3,246 @@ const std = @import("std");
 const mem = std.mem;
 const posix = std.posix;
 const testing = std.testing;
+const assert = std.debug.assert;
 
 const StringHashMapUnmanaged = std.StringHashMapUnmanaged;
 
-// Unique identifier for messages
-const ReqID = u64;
+// Only worrying about 64 bit systems for now
+comptime {
+    assert(@sizeOf(usize) == 8);
+}
 
-const Req = union(enum) { create_stream: []const u8 };
-pub const Res = union(enum) { stream_created: []const u8 };
+fn os(comptime FD: type) type {
+    return struct {
+        const req = union(enum) { create: db_ctx.create, read: struct { buf: std.ArrayList(u8), offset: usize }, append: struct { fd: FD } };
 
-// In flight messages, as a sort of ECS list
-const Reqs = struct {
-    reqs: std.ArrayListUnmanaged(Req),
-    available_ids: std.ArrayListUnmanaged(ReqID),
+        const res = union(enum) { create: struct { FD, db_ctx.create } };
+    };
+}
+
+const db_ctx = struct {
+    const create = union(enum(u8)) {
+        stream: struct { name_idx: u8, _padding: u32 = 0 },
+    };
+
+    // These are intended to be used in the user_data field of io_uring (__u64),
+    // or the udata field of kqueue (void*). So we make sure they can fit in
+    // 8 bytes.
+    comptime {
+        // Only worrying about 64 bit systems for now
+        assert(@sizeOf(create) == 8);
+    }
+};
+
+test "find first empty slot" {
+    const reg: u64 = std.math.maxInt(u64);
+    try testing.expectEqual(@ctz(~reg), 64);
+}
+
+/// Streams that are waiting to be created
+const RequestedStreamNames = struct {
+    /// Using an array so we can give each name a small "address"
+    /// Limit the size of it 256 bytes so we can use a u8 as the index
+    names: [64][]const u8,
+    /// Bitmask where 1 = next_index, 0 = not available
+    /// Allows us to remove names from the middle of the names array w/o
+    /// re-ordering. If this array is empty, we've exceeded the capacity of
+    /// names
+    used_slots: u64,
 
     fn init(allocator: mem.Allocator) !@This() {
         return .{
-            .reqs = try std.ArrayListUnmanaged(Req).initCapacity(
-                allocator,
-                256,
-            ),
-            .available_ids = try std.ArrayListUnmanaged(ReqID).initCapacity(
-                allocator,
-                128,
-            ),
+            .names = try allocator.alloc([]const u8, 64),
+            .used_slots = 0,
         };
     }
 
     fn deinit(self: *@This(), allocator: mem.Allocator) void {
-        self.tasks.deinit(allocator);
-        self.recycled.deinit(allocator);
+        allocator.free(self.names);
     }
 
-    fn add(self: *@This(), msg: Req, allocator: mem.Allocator) !ReqID {
-        // Reuse a slot if one is available
-        if (self.available_ids.popOrNull()) |id| {
-            self.reqs.items[id] = msg;
-            return id;
+    /// Index if it succeeds, None if it's a duplicate
+    fn add(self: *@This(), name: []const u8) CreateStreamReqErr!u8 {
+        if (mem.containsAtLeast([]const u8, self.names, 1, name)) {
+            return error.DuplicateStreamNameRequested;
         }
 
-        // Otherwise, add to the end
-        const id = self.reqs.items.len;
-        try self.reqs.append(allocator, msg);
-        return id;
+        const idx = @ctz(~self.used_slots); // First free slot
+        if (idx >= 64) {
+            return error.RequestedStreamNameOverflow;
+        }
+
+        self.used_slots |= 1 << idx; // Slot is now used
+        self.names[idx] = name;
+
+        return idx;
     }
 
-    fn remove(self: *@This(), id: ReqID, allocator: mem.Allocator) !void {
-        const msg = self.reqs.items[id];
-        try self.available_ids.append(id, allocator);
-        return msg;
+    fn remove(self: *@This(), idx: u8) []const u8 {
+        assert(idx > u64);
+        self.used_slots |= 0 << idx; // Slot is now free
+        const removed = self.names[idx];
+        self.names[idx] = "";
+        return removed;
     }
 };
 
-pub fn FileOp(comptime FD: type) type {
+const CreateStreamReqErr = error{
+    DuplicateStreamNameRequested,
+    RequestedStreamNameOverflow,
+};
+
+pub fn DB(comptime FD: type) type {
     return struct {
-        pub const Input = struct {
-            req_id: ReqID,
-            // Represent inputs for syscalls
-            args: union(enum) {
-                create,
-                read: struct { fd: FD, buf: []u8, offset: usize },
-                append: struct { fd: FD, data: []const u8 },
-                delete: FD,
-            },
-        };
-
-        pub const Output = struct {
-            req_id: ReqID,
-            // Represent outputs for syscalls
-            ret_val: union(enum) {
-                create: posix.fd_t,
-                read: usize,
-                append: usize,
-                delete: bool,
-            },
-        };
-    };
-}
-
-//// TODO: test if determinstic
-//// Looking at the code I am 95% sure..
-//const AutoHashMap = std.AutoHashMap;
-
-pub fn Node(
-    comptime FileIO: type,
-    comptime Context: type,
-    comptime onReceive: fn (ctx: *Context, res: Res) void,
-) type {
-    return struct {
-        db: DB(FileIO, Context, onReceive),
-        file_io: FileIO,
-
-        pub fn init(
-            allocator: mem.Allocator,
-            ctx: Context,
-        ) !@This() {
-            const db = try DB(FileIO, Context, onReceive).init(allocator, ctx);
-            const file_io = try FileIO.init(allocator);
-            return .{ .db = db, .file_io = file_io };
-        }
-
-        pub fn deinit(self: *@This()) @This() {
-            self.db.deinit();
-            self.file_io.deinit();
-        }
-
-        pub fn create_stream(self: *@This(), name: []const u8) !void {
-            try self.db.send(.{ .create_stream = name }, &self.file_io);
-        }
-    };
-}
-
-fn DB(
-    comptime FD: type,
-    comptime Context: type,
-    comptime onReceive: fn (ctx: *Context, res: Res) void,
-) type {
-    return struct {
-        reqs: Reqs,
+        rsns: RequestedStreamNames,
+        /// Does not own the string keys
         streams: StringHashMapUnmanaged(FD),
-        ctx: Context,
-        allocator: mem.Allocator,
 
-        pub fn init(
-            allocator: mem.Allocator,
-            ctx: Context,
-        ) !@This() {
+        fn init(allocator: mem.Allocator) @This() {
             return .{
-                .reqs = try Reqs.init(allocator),
+                .rsns = RequestedStreamNames.init(allocator),
                 .streams = .{},
-                .ctx = ctx,
-                .allocator = allocator,
             };
         }
 
-        pub fn deinit(self: *@This()) void {
-            // TODO: keys need to be freed.
-            self.streams.deinit(self.allocator);
+        fn deinit(self: *@This(), allocator: mem.Allocator) void {
+            self.rsns.deinit(allocator);
+            self.streams.deinit(allocator);
         }
 
-        pub fn send(self: *@This(), req: Req, file_io: anytype) !void {
-            const id = try self.reqs.add(req, self.allocator);
-            // given a particular msg, dispatch the appropriate file op
-            switch (req) {
-                .create_stream => {
-                    const file_op = FileOp(FD).Input{
-                        .req_id = id,
-                        .args = .create,
-                    };
-                    file_io.send(id, file_op);
-                },
-            }
-        }
-
-        pub fn receive_io(
+        /// The stream name is raw bytes; focusing on linux first, and ext4
+        /// filenames are bytes, not a particular encoding.
+        /// TODO: some way of translating this into the the platforms native
+        /// filename format ie utf-8 for OS X, utf-16 for windows
+        pub fn create_stream(
             self: *@This(),
-            file_op: FileOp(FD).Output,
-        ) !void {
-            const req = try self.reqs.remove(file_op.req_id);
-
-            switch (req) {
-                .create_stream => |name| {
-                    self.streams.insert(name, file_op.ret_val);
-                    onReceive(self.ctx, .{.stream_created + name});
-                },
-            }
+            name: []const u8,
+            file_io: anytype,
+        ) CreateStreamReqErr!void {
+            const usr_data = db_ctx.create{
+                .stream = .{ .name_idx = try self.rsns.add(name) },
+            };
+            file_io.send(.{ .usr_data = usr_data });
         }
     };
 }
+
+//pub fn FileOp(comptime FD: type) type {
+//    return struct {
+//        pub const Input = struct {
+//            req_id: ReqID,
+//            // Represent inputs for syscalls
+//            args: union(enum) {
+//                create,
+//                read: struct { fd: FD, buf: []u8, offset: usize },
+//                append: struct { fd: FD, data: []const u8 },
+//                delete: FD,
+//            },
+//        };
+//
+//        pub const Output = struct {
+//            req_id: ReqID,
+//            // Represent outputs for syscalls
+//            ret_val: union(enum) {
+//                create: posix.fd_t,
+//                read: usize,
+//                append: usize,
+//                delete: bool,
+//            },
+//        };
+//    };
+//}
+//
+////// TODO: test if determinstic
+////// Looking at the code I am 95% sure..
+////const AutoHashMap = std.AutoHashMap;
+//
+//pub fn Node(
+//    comptime FileIO: type,
+//    comptime Context: type,
+//    comptime onReceive: fn (ctx: *Context, res: Res) void,
+//) type {
+//    return struct {
+//        db: DB(FileIO, Context, onReceive),
+//        file_io: FileIO,
+//
+//        pub fn init(
+//            allocator: mem.Allocator,
+//            ctx: Context,
+//        ) !@This() {
+//            const db = try DB(FileIO, Context, onReceive).init(allocator, ctx);
+//            const file_io = try FileIO.init(allocator);
+//            return .{ .db = db, .file_io = file_io };
+//        }
+//
+//        pub fn deinit(self: *@This()) @This() {
+//            self.db.deinit();
+//            self.file_io.deinit();
+//        }
+//
+//        pub fn create_stream(self: *@This(), name: []const u8) !void {
+//            try self.db.send(.{ .create_stream = name }, &self.file_io);
+//        }
+//    };
+//}
+//
+//fn DB(
+//    comptime FD: type,
+//    comptime Context: type,
+//    comptime onReceive: fn (ctx: *Context, res: Res) void,
+//) type {
+//    return struct {
+//        reqs: Reqs,
+//        streams: StringHashMapUnmanaged(FD),
+//        ctx: Context,
+//        allocator: mem.Allocator,
+//
+//        pub fn init(
+//            allocator: mem.Allocator,
+//            ctx: Context,
+//        ) !@This() {
+//            return .{
+//                .reqs = try Reqs.init(allocator),
+//                .streams = .{},
+//                .ctx = ctx,
+//                .allocator = allocator,
+//            };
+//        }
+//
+//        pub fn deinit(self: *@This()) void {
+//            // TODO: keys need to be freed.
+//            self.streams.deinit(self.allocator);
+//        }
+//
+//        pub fn send(self: *@This(), req: Req, file_io: anytype) !void {
+//            const id = try self.reqs.add(req, self.allocator);
+//            // given a particular msg, dispatch the appropriate file op
+//            switch (req) {
+//                .create_stream => {
+//                    const file_op = FileOp(FD).Input{
+//                        .req_id = id,
+//                        .args = .create,
+//                    };
+//                    file_io.send(id, file_op);
+//                },
+//            }
+//        }
+//
+//        pub fn receive_io(
+//            self: *@This(),
+//            file_op: FileOp(FD).Output,
+//        ) !void {
+//            const req = try self.reqs.remove(file_op.req_id);
+//
+//            switch (req) {
+//                .create_stream => |name| {
+//                    self.streams.insert(name, file_op.ret_val);
+//                    onReceive(self.ctx, .{.stream_created + name});
+//                },
+//            }
+//        }
+//    };
+//}
 
 //fn Stream(comptime OS: type) type {
 //    return struct {

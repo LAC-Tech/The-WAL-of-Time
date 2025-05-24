5a84ae9d6c57ca9d1d30855035ca1f1808b88508
Lewis Campbell <info@lewiscampbell.tech>
2025-05-24 18:37:44 +1200

    updated to match the zig

--- src/lib.rs
+++ src/lib.rs
@@ -1,87 +1,61 @@
 #![no_std]
 
-#[macro_use]
 extern crate alloc;
-
 use core::mem;
 
-// Only worrying about 64 bit systems for now
-const _: () = assert!(mem::size_of::<usize>() == 8);
+use foldhash::fast::FixedState;
+use hashbrown::{HashMap, hash_map};
 
-pub mod file_io {
-    use super::db_ctx;
-    use alloc::boxed::Box;
-    use alloc::vec::Vec;
-    pub trait AsyncFS {
-        type FD;
-        fn send(&mut self, req: Req<Self::FD>);
-    }
+// Kqueue's udata and io_uring's user_data are a void* and _u64 respectively
+const _: () = assert!(mem::size_of::<*mut core::ffi::c_void>() == 8);
 
-    /// Requests: user -> node #[repr(u8)]
+// Messages sent to an async file system with a req/res interface
+mod fs {
+    use crate::topic;
+
     pub enum Req<FD> {
-        Create(db_ctx::Create),
-        Read { buf: Vec<u8>, offset: usize },
-        Append { fd: FD, data: Box<[u8]> },
-        Delete(FD),
+        Create { topic_id: topic::ID },
+        //Read { fd: FD },
+        //Append { fd: FD },
+        Delete { fd: FD },
     }
 
-    /// Response: node -> user
     pub enum Res<FD> {
-        Create(FD, db_ctx::Create),
-        Read(usize),
-        Append(usize),
-        Delete(bool),
+        Create { fd: FD, topic_id: topic::ID },
+        //Read,
+        //Append,
+        //Delete,
     }
 }
 
-/// Database Context
-/// Small bits of data so the DB knows what an IO req was for when it gets the
-/// response back
-mod db_ctx {
-    #[repr(u8)]
-    pub enum Create {
-        Stream { name_idx: u8, _padding: u32 },
+/// Top Level Response, for the user of the library
+mod usr {
+    pub enum Req<'a> {
+        CreateTopic { name: &'a [u8] },
     }
 
-    // These are intended to be used in the user_data field of io_uring (__u64),
-    // or the udata field of kqueue (void*). So we make sure they can fit in
-    // 8 bytes.
-    const _: () = {
-        assert!(core::mem::size_of::<Create>() == 8);
-    };
+    pub enum Res<'a> {
+        TopicCreated { name: &'a [u8] },
+    }
 }
 
-pub mod db {
+mod topic {
     use alloc::boxed::Box;
-    use alloc::string::String;
-    use foldhash::fast::FixedState;
-    use hashbrown::HashMap;
 
-    use crate::{db_ctx, file_io};
-
-    //use super::file_io::AsyncFileIO;
-
-    /// User Response
-    #[derive(Debug)]
-    pub enum URes<'a> {
-        StreamCreated { name: &'a [u8] },
-    }
-
-    pub trait UserCtx {
-        fn send(&mut self, res: URes<'_>);
-    }
+    pub struct ID(u8);
+    const MAX: u8 = 64;
 
     #[derive(Debug)]
-    pub enum CreateStreamErr {
+    pub enum CreateErr {
         DuplicateName,
         ReservationLimitExceeded,
     }
 
-    /// Streams that are waiting to be created
-    struct RequestedStreamNames<'a> {
+    /// Topics that are waiting to be created
+    pub struct RequestedNames<'a> {
         /// Using an array so we can give each name a small "address"
-        /// Limit the size of it 256 bytes so we can use a u8 as the index
-        names: Box<[&'a [u8]; 64]>,
+        /// Ensure size is less than 256 bytes so we can use a u8 as the index
+        names: Box<[&'a [u8]; MAX as usize]>,
         /// Bitmask where 1 = occupied, 0 = available
         /// Allows us to remove names from the middle of the names array w/o
         /// re-ordering. If this array is empty, we've exceeded the capacity of
@@ -89,90 +63,95 @@ pub mod db {
         used_slots: u64,
     }
 
-    impl<'a> RequestedStreamNames<'a> {
-        fn new() -> Self { Self { names: Box::new([b""; 64]), used_slots: 0 } }
+    impl<'a> RequestedNames<'a> {
+        pub fn new() -> Self {
+            Self { names: Box::new([b""; 64]), used_slots: 0 }
+        }
 
-        /// Index if it succeeds, None if it's a duplicate
-        fn add(&mut self, name: &'a [u8]) -> Result<u8, CreateStreamErr> {
+        pub fn add(&mut self, name: &'a [u8]) -> Result<ID, CreateErr> {
             if self.names.contains(&name) {
-                return Err(CreateStreamErr::DuplicateName)
+                return Err(CreateErr::DuplicateName)
             }
 
             // Find first free slot
             let idx = (!self.used_slots).trailing_zeros() as u8;
-            if idx >= 64 {
-                return Err(CreateStreamErr::ReservationLimitExceeded);
+            if idx >= MAX {
+                return Err(CreateErr::ReservationLimitExceeded);
             }
 
             // Mark slot as used
             self.used_slots |= 1u64 << idx;
             self.names[idx as usize] = name;
 
-            Ok(idx)
+            Ok(ID(idx))
         }
 
-        fn remove(&mut self, index: u8) -> &'a [u8] {
-            assert!(index < 64, "Index out of bounds");
-            self.used_slots &= !(1u64 << index);
-            core::mem::take(&mut self.names[index as usize])
+        pub fn remove(&mut self, topic_id: ID) -> &'a [u8] {
+            assert!(topic_id.0 < MAX, "Index out of bounds");
+            self.used_slots &= !(1u64 << topic_id.0);
+            core::mem::take(&mut self.names[topic_id.0 as usize])
+        }
+    }
+}
+
+/// This structs job is to receive "completed" async fs events, and:
+/// 1 - update reflect changs to the node in memory and
+/// 2 - return a meaningful response for user code
+/// It's completey decoupled from any async runtime
+pub struct Node<'a, FD> {
+    reqd_topic_names: topic::RequestedNames<'a>,
+    topic_fds: HashMap<&'a [u8], FD, FixedState>, // Deterministic Hashmap
+}
+
+impl<'a, FD> Node<'a, FD> {
+    pub fn new(seed: u64) -> Self {
+        Self {
+            reqd_topic_names: topic::RequestedNames::new(),
+            topic_fds: HashMap::with_hasher(FixedState::with_seed(seed)),
         }
     }
 
-    pub struct DB<'a, FD> {
-        rsn: RequestedStreamNames<'a>,
-        /// Determistic Hashmap for testing
-        streams: HashMap<&'a [u8], FD, FixedState>,
-    }
+    /// The topic name is raw bytes; focusing on linux first, and ext4
+    /// filenames are bytes, not a particular encoding.
+    /// TODO: some way of translating this into the the platforms native
+    /// filename format ie utf-8 for OS X, utf-16 for windows
+    pub fn req_usr_to_fs(
+        &mut self,
+        usr_req: usr::Req<'a>,
+    ) -> Result<fs::Req<FD>, topic::CreateErr> {
+        match usr_req {
+            usr::Req::CreateTopic { name } => {
+                if self.topic_fds.contains_key(name) {
+                    return Err(topic::CreateErr::DuplicateName)
+                }
 
-    impl<'a, FD> DB<'a, FD> {
-        pub fn new(seed: u64) -> Self {
-            Self {
-                rsn: RequestedStreamNames::new(),
-                streams: HashMap::with_hasher(FixedState::with_seed(seed)),
+                let topic_id = self.reqd_topic_names.add(name)?;
+
+                Ok(fs::Req::Create { topic_id })
             }
         }
+    }
 
-        /// The stream name is raw bytes; focusing on linux first, and ext4
-        /// filenames are bytes, not a particular encoding.
-        /// TODO: some way of translating this into the the platforms native
-        /// filename format ie utf-8 for OS X, utf-16 for windows
-        pub fn create_stream(
-            &mut self,
-            name: &'a [u8],
-            file_io: &mut impl file_io::AsyncFS<FD = FD>,
-        ) -> Result<(), CreateStreamErr> {
-            let name_idx = self.rsn.add(name)?;
-            let usr_data = db_ctx::Create::Stream { name_idx, _padding: 0 };
-            file_io.send(file_io::Req::Create(usr_data));
-            Ok(())
-        }
-
-        pub fn receive_io(
-            &mut self,
-            res: file_io::Res<FD>,
-            usr_ctx: &mut impl UserCtx,
-        ) {
-            let usr_res = match res {
-                file_io::Res::Create(
-                    fd,
-                    db_ctx::Create::Stream { name_idx, _padding },
-                ) => {
-                    let name = self.rsn.remove(name_idx);
-                    let prev_val = self.streams.insert(name, fd);
-                    assert!(
-                        prev_val.is_none(),
-                        "Duplicate Stream Name :{}",
-                        String::from_utf8_lossy(name)
-                    );
-                    URes::StreamCreated { name }
+    /// This turns internal DB and async io stuff into something relevant
+    /// to the end user.
+    /// It is one function, rather than one for each case, because I
+    /// envison the result of this having a single callback associated with
+    /// it in user code.
+    /// TODO: review these assumptions
+    pub fn res_fs_to_usr(&mut self, fs_res: fs::Res<FD>) -> usr::Res {
+        match fs_res {
+            fs::Res::Create { fd, topic_id } => {
+                let name = self.reqd_topic_names.remove(topic_id);
+                match self.topic_fds.entry(name) {
+                    hash_map::Entry::Vacant(entry) => {
+                        entry.insert(fd);
+                    }
+                    hash_map::Entry::Occupied(_) => {
+                        panic!("failed to reserve topic name")
+                    }
                 }
-
-                _ => {
-                    panic!("TODO: handle more io requests");
-                }
-            };
-
-            usr_ctx.send(usr_res);
+                usr::Res::TopicCreated { name }
+            }
         }
     }
 }

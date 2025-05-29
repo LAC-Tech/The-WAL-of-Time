3ed4ecbf6ea949d7557de3e6e5958622835faa48
Lewis Campbell <info@lewiscampbell.tech>
2025-05-29 22:50:01 +1200

    the dumbest and most basic of io_uring socket loops

--- build.zig
+++ build.zig
@@ -46,7 +46,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "dst",
-        .root_source_file = b.path("src/dst.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });

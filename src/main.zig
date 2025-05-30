d7d568fd696487564499805dc725fb362bbc8838
Lewis Campbell <info@lewiscampbell.tech>
2025-05-30 21:12:47 +1200

    some more states, acknowledging the connection

--- src/main.zig
+++ src/main.zig
@@ -15,18 +15,20 @@ pub fn main() !void {
     );
     defer posix.close(socket_fd);
 
-    const port = 12345;
-    var addr = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, port);
-    var addr_len: posix.socklen_t = addr.getOsSockLen();
-
     try posix.setsockopt(
         socket_fd,
         posix.SOL.SOCKET,
         posix.SO.REUSEADDR,
-        // TODO wtf is this again? looks like a magic number
+        // Man page: "For Boolean options, 0 indicates that the option is
+        // disabled and 1 indicates that the option is enabled."
         &std.mem.toBytes(@as(c_int, 1)),
     );
+
+    const port = 12345;
+    var addr = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, port);
+    var addr_len: posix.socklen_t = addr.getOsSockLen();
     try posix.bind(socket_fd, &addr.any, addr_len);
+
     const backlog = 128;
     try posix.listen(socket_fd, backlog);
 
@@ -37,7 +39,6 @@ pub fn main() !void {
         &addr_len,
         0,
     );
-
     debug.assert(try ring.submit() == 1);
 
     debug.print("The WAL weaves as the WAL wills\n", .{});
@@ -54,8 +55,7 @@ pub fn main() !void {
 
         switch (usr_data) {
             .connect => {
-                debug.print("accept received \n", .{});
-                //const client_fd = cqe.res;
+                const client_fd = cqe.res;
                 //posix.close(client_fd);
 
                 _ = try ring.accept(
@@ -66,7 +66,20 @@ pub fn main() !void {
                     0,
                 );
 
+                // TODO: wait until the recv has come through to do this
+                _ = try ring.send(
+                    @intFromEnum(core.UsrData.todo),
+                    client_fd,
+                    "connection acknowledged\n",
+                    0,
+                );
+
+                // TODO: "recv" here, so that this client can send us data
+
                 _ = try ring.submit();
+            },
+            .todo => {
+                debug.print("todo\n", .{});
             },
         }
     }
@@ -80,5 +93,5 @@ pub fn main() !void {
 // - inner loop that batch processes all ready CQE events, like TB blog
 
 const core = struct {
-    const UsrData = enum(u64) { connect };
+    const UsrData = enum(u64) { connect, todo };
 };

742a5dd7ea3ebe82e12b1d40e3d20bc98b249de7
Lewis Campbell <info@lewiscampbell.tech>
2025-06-03 16:58:26 +1200

    Revert "there ARE limits"
    This reverts commit b72d02e47704877eb6da21aa603b9e028fc965bf.

--- src/async_io_msg.zig
+++ src/async_io_msg.zig
@@ -1,10 +1,7 @@
-// This is very POSIX centric
-
-pub const Res = struct { rc: i32, usr_data: u64 };
+pub const res = struct { rc: i32, usr_data: u64 };
 
 pub fn req(comptime FD: type) type {
     return struct {
-        pub const Accept = u64;
         pub const Send = struct { usr_data: u64, client_fd: FD, buf: []const u8 };
         pub const Recv = struct { usr_data: u64, client_fd: FD, buf: []u8 };
     };
--- src/limits.zig
+++
@@ -1,2 +0,0 @@
-pub const max_clients: usize = 2;
-pub const write_buf_size: usize = 64;

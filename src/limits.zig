b72d02e47704877eb6da21aa603b9e028fc965bf
Lewis Campbell <info@lewiscampbell.tech>
2025-06-03 14:36:16 +1200

    there ARE limits

---
+++ src/limits.zig
@@ -0,0 +1,2 @@
+pub const max_clients: usize = 2;
+pub const write_buf_size: usize = 64;
--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,9 @@ const net = std.net;
 const debug = std.debug;
 const mem = std.mem;
 
-const linux = @import("./linux.zig");
 const core = @import("./core.zig");
+const limits = @import("./limits.zig");
+const linux = @import("./linux.zig");
 
 pub fn main() !void {
     var aio = try linux.AsyncIO.init();
@@ -18,65 +19,38 @@ fn event_loop(aio: anytype) !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var rt = try core.RunTime(
-        linux.fd_t,
-        linux.fd_eql,
-        max_clients,
-        write_buf_size,
-    ).init(allocator);
+    var rt = try core.RunTime(linux.fd_t, linux.fd_eql).init(allocator);
     defer rt.deinit(allocator);
 
-    for (0..max_clients) |_| {
+    for (0..limits.max_clients) |_| {
         _ = try aio.accept(core.UsrData.client_connected);
     }
-    debug.assert(try aio.flush() == max_clients);
+    debug.assert(try aio.flush() == limits.max_clients);
 
     debug.print("The WAL weaves as the WAL wills\n", .{});
 
     while (true) {
         const aio_res = try aio.wait_for_res();
 
-        // TODO: give aio_res directly to core and return tagged enum annotated
-        // with data needed to do further dispatches on the aio
-        const usr_data: core.UsrData = @bitCast(aio_res.usr_data);
-
-        switch (usr_data.tag) {
-            .client_connected => {
-                const client_fd: linux.fd_t = aio_res.rc;
-                const send_req = try rt.register_client(client_fd);
-
+        switch (try rt.process_aio_res(aio_res)) {
+            .client_connected => |aio_reqs| {
                 // Replace itself on the queue, so other clients can connect
-                _ = try aio.accept(core.UsrData.client_connected);
-
+                _ = try aio.accept(aio_reqs.accept);
                 // Let client know they can connect.
-                _ = try aio.send(send_req);
+                _ = try aio.send(aio_reqs.send);
 
                 debug.assert(2 == try aio.flush());
             },
-            .client_ready => {
-                const client_slot = usr_data.payload.client_slot;
-                const rt_res = rt.prepare_client(client_slot);
-
-                // so we can receive a message
-                _ = try aio.recv(rt_res);
-
+            .client_ready => |recv| {
+                // so we can receive more a message
+                _ = try aio.recv(recv);
                 debug.assert(1 == try aio.flush());
             },
-            .client_msg => {
-                const client_slot = usr_data.payload.client_slot;
-
-                debug.print("received: {s}", .{rt.recv_buf});
-
-                const rt_res = rt.prepare_client(client_slot);
-
+            .client_msg => |recv| {
                 // So we can receive more messages
-                _ = try aio.recv(rt_res);
-
+                _ = try aio.recv(recv);
                 debug.assert(1 == try aio.flush());
             },
         }
     }
 }
-
-const max_clients = 2; // TODO: more
-const write_buf_size = 64;
